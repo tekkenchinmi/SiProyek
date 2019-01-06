4d3786ec3de2e079760021423350076ec567af31
atjeprahmat <32193615+atjeprahmat@users.noreply.github.com>
2019-01-06 10:30:34 +0700

    upload index

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>SiProyek</title>
+
+    <!-- menyisipkan bootstrap -->
+     <link rel="stylesheet" href="css/bootstrap.min.css" />
+</head>
+<body class="bg-light">
+    <header>
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-11">
+                        <h1>Sistem Informasi Manajemen Proyek</h1>
+                        <p><h2>PT Madani Intelsysdata</h2></p>
+                        <p><h5>Business Intellegence Solutions</h5></p>
+                    </div>
+                    <div class="col-md-8">
+                        <a href="login.php" target="newtab" class="btn btn-success">Login</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <!--<img class="img img-responsive" src="img/connect.png" />-->
+                </div>
+            </div>
+        </div>
+    </section>
+
+</body>
+</html>
